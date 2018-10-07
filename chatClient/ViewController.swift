650f82ac10553ac7909f69e1cb7ab732d6b007d4
Matthew <Matthew.b.rodriguez@gmail.com>
2018-10-06 20:48:05 -0400

    Initial Commit

---
+++ chatClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  chatClient
+//
+//  Created by Matthew on 10/6/18.
+//  Copyright © 2018 Matthew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
